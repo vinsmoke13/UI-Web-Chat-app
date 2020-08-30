2e2aa28e9678655ada997b5789b0fc3038833e3b
vinsmoke13 <vish13.chandel@gmail.com>
2020-08-31 00:42:02 +0530

    front end UI for chat app

---
+++ script.js
@@ -0,0 +1,51 @@
+$('.friend-drawer--onhover').on('click', function() {
+  $('.chat-bubble').hide().show();
+});
+
+$("#scrollbottom").scrollTop($("#scrollbottom")[0].scrollHeight);
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick=function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns=document.getElementsByClassName("dropdown-content");
+    var i;
+    for(i=0;i<dropdowns.length;i++) {
+      var openDropdown=dropdowns[i];
+      if(openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns=document.getElementsByClassName("dropdown-content");
+    var i;
+    for(i=0;i<dropdowns.length;i++) {
+      var openDropdown=dropdowns[i];
+      if(openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function send()
+{
+   document.getElementById('dis').innerHTML=document.myform.name.value;
+}
+
+function showmg() {
+  document.getElementById('di').style.display = "block";
+}
+
+function res() { 
+  /*Put all the data posting code here*/ 
+ document.getElementById("nfor").reset(); 
+} 
